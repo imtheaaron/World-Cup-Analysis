43f62aa3d1a924da9629638b902b6da3dfb6b886
Aaron Burke <35472931+imtheaaron@users.noreply.github.com>
2018-07-09 22:28:39 -0700

    cleaned up the map code, minor bootstrap changes to index.html

--- static/js/map.js
+++ static/js/map.js
@@ -12,54 +12,35 @@ d3.json('/rankings/', function(data) {
     rankings = data;
 });
 
-// function buildmap() {
-//     var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
-//         mapboxKey);
-
-//     var baseMaps = {
-//         "Light Map": lightMap,
-//     };
-
-//     var myMap = L.map("map", {
-//         center: [30.3429959, -5.8608298],
-//         zoom: 3,
-//         layers: [lightMap] // borders, fifaColors]
-//     });
-
-// };
-
 //build the initial map (1994)
 function init() {
     // function to build the map with 1994 data
     let year = '1994';
-    selector();
+    // selector();
     makeMap(year);
     // makeMetaData(year);
     // scatter(year);
         // slider(); 
 };
 
-function selector() {
-    var wcYears = ['1994', '1998', '2002', '2006', '2010', '2014', '2018'];
+// function selector() {
+//     var wcYears = ['1994', '1998', '2002', '2006', '2010', '2014', '2018'];
 
-    var select = document.getElementById("yearSet");
-    for(var i = 0; i < wcYears.length; i++) {
-        var id = wcYears[i];
-        var el = document.createElement("option");
-        el.textContent = id;
-        el.value = id;
-        select.appendChild(el);
-    }
-};
+//     var select = document.getElementById("yearSet");
+//     for(var i = 0; i < wcYears.length; i++) {
+//         var id = wcYears[i];
+//         var el = document.createElement("option");
+//         el.textContent = id;
+//         el.value = id;
+//         select.appendChild(el);
+//     }
+// };
 
 // function to grab a year from the slider and recreate the map from that year data
 function yearSelected(value) {
     var year = value;
-    //INSTEAD OF REMAKING THE MAP HERE, JUST REMOVE A LAYER FROM THE LAYER GROUP WITH removeLayer()
-    //THEN HAVE A FUNCTION THAT WILL CREATE A NEW LAYER WITH THE NEW DATA (also clearLayers() )
     reColorMap(year);
     // makeMetaData(year);
-    // scatter(year)
 };
  
 // create the map with country colors for the specified year
@@ -116,16 +97,12 @@ function makeMap(year) {
         mapboxKey);
 
         var baseMaps = {
-            // "Dark Map": darkMap,
-            // "Light Map": lightMap,
             "Fifa Ranking": fifaColors,
             "World Cup Result": cupColors
         };
 
         var overlayMaps = {
             "Cup Qualifiers": cupQualifiers,
-            // "Fifa Ranking": fifaColors,
-            // "World Cup Result": cupColors
         };
 
         var myMap = L.map("map", {
@@ -187,14 +164,6 @@ function makeMap(year) {
             return div;
         };
         legend.addTo(myMap);
-
-
-
-    //if we do code to create metadata about the particular world cup
-    // let url = "/cup_data/" + year;
-    // d3.json(url, function(error,response) {
-    //     var data = response;
-    // });
 
     });
 
@@ -322,9 +291,6 @@ function reColorMap(year) {
         });
         layer.bindPopup("<h3>" + countryInfo(layer.feature.id, year)[0] + "</h3><p>" + "Confederation: " + countryInfo(layer.feature.id, year)[1] +
         "<hr>" + year + " FIFA Ranking: " + countryInfo(layer.feature.id, year)[2] + "<br>" + year + " WC Result: " + countryInfo(layer.feature.id, year)[3] + "</p>");
-        // var idLayer = layer._leaflet_id;
-        // var country = layer.feature.id;
-        // map._layers[idLayer].setStyle({weight: cupBorder(country, year)});
     });
         
     fifaColors.eachLayer(function (layer) {
@@ -344,17 +310,5 @@ function reColorMap(year) {
     });
 
 };
-
-// function slider() {
-//     // LOGIC TO BUILD SLIDER GOES HERE
-//     var years = [
-//         '1994',
-//         '1998',
-//         '2002',
-//         '2006',
-//         '2010',
-//         '2014',
-//         '2018'
-//     ];
 
 init();
